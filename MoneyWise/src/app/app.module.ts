a799cf905ef5065aca8a3027e9f87d4770345f48
Juan Barraza <juanbarraza2004@outlook.com>
2026-03-04 17:08:51 -0500

    feat: made interceptor to request

--- MoneyWise/src/app/app.module.ts
+++ MoneyWise/src/app/app.module.ts
@@ -7,13 +7,23 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core-module';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { IonicStorageModule } from '@ionic/storage-angular';
+import { InterceptorService } from './core/interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, CoreModule, HttpClientModule, IonicStorageModule.forRoot(),],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptorService,
+      multi: true
+    }],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ MoneyWise/src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,33 @@
+import { StorageService } from 'src/app/core/services/storageService/storage';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { from, Observable, switchMap } from "rxjs";
+
+
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class InterceptorService implements HttpInterceptor {
+  constructor(private storage: StorageService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return from(this.storage.get('token')).pipe(
+      switchMap(token => {
+        if (!token) {
+          return next.handle(req);
+        }
+
+        const authReq = req.clone({
+          headers: req.headers
+            .set('Authorization', `Bearer ${token}`)
+            .set('Client', 'app')
+        });
+
+        return next.handle(authReq);
+      })
+    );
+  }
+}
