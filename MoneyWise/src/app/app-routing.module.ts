55e6ceb99425eda5e74bbc1cfd07fd280f61dd03
Juan Barraza <juanbarraza2004@outlook.com>
2026-03-06 12:45:17 -0500

    fix: resolve redirect in path

--- MoneyWise/src/app/app-routing.module.ts
+++ MoneyWise/src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
     loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
   },
   {
-    path: '',
+    path: '**',
     redirectTo: 'auth/login',
     pathMatch: 'full'
   },
