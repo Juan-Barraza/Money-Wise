8ec08c06e0c69d5d61c1ac6f72af3ba2a6e0da98
Juan Barraza <juanbarraza2004@outlook.com>
2026-03-07 20:09:40 -0500

    fix: improve how to show the amount in the screen

--- MoneyWise/src/app/shared/components/input/input.component.ts
+++ MoneyWise/src/app/shared/components/input/input.component.ts
@@ -11,9 +11,26 @@ export class InputComponent implements OnInit {
   @Input() label: string = '';
   @Input() placeholder: string = '';
   @Input() type: string = 'text';
+  @Input() step: string = '0.01';
   @Input() control: FormControl | any;
+  public displayValue: string = '';
   constructor() { }
 
   ngOnInit() { }
 
+  public onInput(event: any) {
+    const raw = event.target.value;
+    const onlyNumbers = raw.replace(/\D/g, '');
+    if (!onlyNumbers) {
+      this.displayValue = '';
+      this.control.setValue(null);
+      return;
+    }
+    console.log(onlyNumbers);
+    const formated = new Intl.NumberFormat('es-CO').format(Number(onlyNumbers))
+    console.log("formated: ", formated)
+    this.displayValue = formated;
+
+    this.control.setValue(Number(onlyNumbers));
+  }
 }
